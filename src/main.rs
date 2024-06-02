5fe83f6447e33d8213580854769977ee2c3075ce
harm <el.harmo@gmail.com>
2024-06-02 13:16:36 +0200

    Can start an AP, can send register contents to client, now hook up the web stuff

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@
 mod assembly;
 
 use std::ops::BitOr;
-use heapless;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
     hal::prelude::Peripherals,
-    wifi::{AccessPointConfiguration,Configuration, BlockingWifi, EspWifi, Protocol::*, AuthMethod},
+    wifi::{
+        AccessPointConfiguration, AuthMethod, BlockingWifi, Configuration, EspWifi, Protocol::*,
+    },
 };
+use heapless;
 
 use assembly::Interpreter;
 
@@ -29,7 +36,11 @@ fn main() {
         ssid_hidden: false,
         channel: 0,
         secondary_channel: Some(1),
-        protocols: P802D11B.bitor(P802D11BG).bitor(P802D11BGN).bitor(P802D11BGNLR).bitor(P802D11LR),
+        protocols: P802D11B
+            .bitor(P802D11BG)
+            .bitor(P802D11BGN)
+            .bitor(P802D11BGNLR)
+            .bitor(P802D11LR),
         auth_method: AuthMethod::None,
         password: heapless::String::new(),
         max_connections: u16::MAX,
@@ -39,22 +50,52 @@ fn main() {
     let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sysloop).unwrap(); //TODO or non-blocking?
     wifi.start().unwrap();
 
-    let program = vec!["addi x31, x31, 1", "add x31, x31, x31"];
+    let listener = TcpListener::bind("0.0.0.0:80").unwrap();
 
-    let interpreter = Interpreter::new(program);
-
-    match interpreter {
-        Ok(mut interpreter) => {
-            interpreter.step();
-
-            println!("After step 1:\n{:?}\n", interpreter.registers);
-
-            interpreter.step();
-
-            println!("After step 2:\n{:?}\n", interpreter.registers);
-        }
-        Err(e) => println!("Error:\n{e}\n"),
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
 
-    loop {}
+    fn handle_connection(mut stream: TcpStream) {
+        let buf_reader = BufReader::new(&mut stream);
+        let http_request: Vec<_> = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+
+        let program = vec!["addi x31, x31, 1", "add x31, x31, x31"];
+        let mut interpreter = Interpreter::new(program).unwrap();
+
+        interpreter.step();
+
+        println!("After step 1:\n{:?}\n", interpreter.registers);
+
+        interpreter.step();
+
+        println!("After step 2:\n{:?}\n", interpreter.registers);
+
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello!</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <p>Hi from Rust on the ESP32</p>
+    <p>{:?}<p>
+  </body>
+</html>"#,
+            interpreter.registers
+        );
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
